3bcf32d7c35de5f8b9845a27ac76584cecaa4ead
Denis Siliukov <abhem21rus@mail.ru>
2024-11-03 17:57:15 +0300

    Init

---
+++ src/test/java/com/gridnine/testing/FlightFilter/FlightFilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gridnine.testing.FlightFilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightFilterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
